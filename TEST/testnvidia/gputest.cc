d0b98d64e93f04d78b6292862901943f43aa5cc2
martyitz <itzkowitzmarty@gmail.com>
2021-11-14 15:59:01 -0600

    Tweak code to make behavior more obvious.  Support OMP_NUM_THREADS= 1 or 2 in testnvidia

--- TEST/testnvidia/gputest.cc
+++ TEST/testnvidia/gputest.cc
@@ -1,13 +1,15 @@
 #include <cstdio>
 #include <cstdlib>
+#include <math.h>
 #include <omp.h>
 
 #ifdef USE_MPI
 #include <mpi.h>
 #endif
 
-#define	N 160000
-#define	NITER 1000
+#define	N 10000000
+#define	NITER 3
+#define	INNERITER 2
 
 void output(double *p, size_t size, const char *label);
 void init(double *p, size_t size);
@@ -19,6 +21,7 @@ double r1[N], r2[N];
 double p1[N], p2[N];
 size_t nn = N;
 int niter = NITER;
+int omp_num_t;
 
 int
 main(int argc, char *argv[], char **envp)
@@ -52,7 +55,8 @@ main(int argc, char *argv[], char **envp)
     exit(1);
   } else {
     printf("### gputest is able to use the GPU! idev = %d, runningOnGpU -- omp_is_initial_device()\n", idev );
-    printf("-- launching omp target loops on two CPU threads\n" );
+    omp_num_t = omp_get_max_threads();
+    printf("-- launching omp target loops on %d CPU thread%s\n", omp_num_t, omp_num_t ==1? "":"s" );
   }
 
 #ifdef USE_MPI
@@ -74,7 +78,9 @@ main(int argc, char *argv[], char **envp)
 
   }
   output(p1, nn, "p1");
-  output(p2, nn, "p2");
+  if ( omp_num_t != 1) {
+    output(p2, nn, "p2");
+  }
 
 #ifdef USE_MPI
   MPI_Finalize();
@@ -107,7 +113,10 @@ t1work()
         {
           #pragma omp distribute parallel for
           for (size_t i = 0; i < nn; ++i) {
-            p1[i] = l1[i] + r1[i];
+	    for (int j = 0; j < INNERITER; j++ ) {
+              p1[i] = sqrt( exp( log (l1[i]*l1[i]) ) + exp( log (r1[i]*r1[i]) ) /
+                exp( log(l2[i]*l2[i]) ) + exp( log( (r2[i]*r2[i]) )) );
+	    }
           }
         }
         }
@@ -119,17 +128,13 @@ t2work()
         #pragma omp target data map(to:l2[0:nn], r2[0:nn]) map(tofrom: p2[0:nn])
         {
         #pragma omp target
-        #pragma omp teams distribute parallel for num_teams(4) thread_limit(64)
-/*
         #pragma omp teams num_teams(4) thread_limit(64)
         {
           #pragma omp distribute parallel for
-*/
           for (size_t i = 0; i < nn; ++i) {
-            p2[i] = l2[i] + r2[i];
+            p2[i] = sqrt( exp( log (l1[i]*l1[i]) ) + exp( log (r1[i]*r1[i])) ) /
+              sqrt( exp( log(l2[i]*l2[i]) ) + exp( log( (r2[i]*r2[i]) )) );
           }
-/*
         }
-*/
         }
 }
