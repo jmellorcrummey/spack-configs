db96a702ae58e2c297ff9c1b6edada767309236b
Wileam Yonatan Phan <wil.phan@rice.edu>
2024-02-22 13:49:42 -0600

    ufront Feb 2024 update

---
+++ rice/ufront/ufront.gcc-module-template.tcl
@@ -0,0 +1,84 @@
+#%Module1.0
+## Module file created by spack (https://github.com/spack/spack) on {{ timestamp }}
+##
+## {{ spec.short_spec }}
+##
+{% if configure_options %}
+## Configure options: {{ configure_options }}
+##
+{% endif %}
+
+
+{% block header %}
+{% if short_description %}
+module-whatis {{ '{' }}{{ short_description }}{{ '}' }}
+{% endif %}
+
+proc ModulesHelp { } {
+    puts stderr {{ '{' }}Name   : {{ spec.name }}{{ '}' }}
+    puts stderr {{ '{' }}Version: {{ spec.version }}{{ '}' }}
+    puts stderr {{ '{' }}Target : {{ spec.target }}{{ '}' }}
+{% if long_description %}
+    puts stderr {}
+{{ long_description| textwrap(72)| curly_quote()| prepend_to_line('    puts stderr ')| join() }}
+{% endif %}
+}
+{% endblock %}
+
+# Emulate Lmod "family" behavior
+{% if {{ spec.name }} == 'gcc' %}
+{% endif %}
+
+{% block autoloads %}
+{% if autoload|length > 0 %}
+if {![info exists ::env(LMOD_VERSION_MAJOR)]} {
+{% for module in autoload %}
+    module load {{ module }}
+{% endfor %}
+} else {
+{% for module in autoload %}
+    depends-on {{ module }}
+{% endfor %}
+}
+{% endif %}
+{% endblock %}
+{#  #}
+{% block prerequisite %}
+{% for module in prerequisites %}
+prereq {{ module }}
+{% endfor %}
+{% endblock %}
+{#  #}
+{% block conflict %}
+{% for name in conflicts %}
+conflict {{ name }}
+{% endfor %}
+{% endblock %}
+
+{% block environment %}
+{% for command_name, cmd in environment_modifications %}
+{% if command_name == 'PrependPath' %}
+prepend-path --delim {{ '{' }}{{ cmd.separator }}{{ '}' }} {{ cmd.name }} {{ '{' }}{{ cmd.value }}{{ '}' }}
+{% elif command_name in ('AppendPath', 'AppendFlagsEnv') %}
+append-path --delim {{ '{' }}{{ cmd.separator }}{{ '}' }} {{ cmd.name }} {{ '{' }}{{ cmd.value }}{{ '}' }}
+{% elif command_name in ('RemovePath', 'RemoveFlagsEnv') %}
+remove-path --delim {{ '{' }}{{ cmd.separator }}{{ '}' }} {{ cmd.name }} {{ '{' }}{{ cmd.value }}{{ '}' }}
+{% elif command_name == 'SetEnv' %}
+setenv {{ cmd.name }} {{ '{' }}{{ cmd.value }}{{ '}' }}
+{% elif command_name == 'UnsetEnv' %}
+unsetenv {{ cmd.name }}
+{% endif %}
+{#  #}
+{% endfor %}
+{# Make sure system man pages are enabled by appending trailing delimiter to MANPATH #}
+{% if has_manpath_modifications %}
+append-path --delim {{ '{' }}:{{ '}' }} MANPATH {{ '{' }}{{ '}' }}
+{% endif %}
+{% endblock %}
+
+{% block footer %}
+{# In case the module needs to be extended with custom Tcl code #}
+{% if {{ spec.name }} == 'gcc' %}
+module use /projects/deploy/moduleroot/{{ spec.name }}-{{ spec.version }}
+{% endif %}
+{% endblock %}
